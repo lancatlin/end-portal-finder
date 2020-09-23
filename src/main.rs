5b16d35d85d15c28f204c54fd9ffe371e2f5a529
wancat <wancat@pm.me>
2020-09-23 14:33:15 +0800

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,28 @@ use std::io::{self, Write};
 
 fn main() {
     println!("Please input the coordinate!");
-	println!("syntax: a.x a.y b.x b.y");
-	println!("Press Ctrl+C to exit.");
+    println!("syntax: a.x a.y b.x b.y");
+    println!("Press Ctrl+C to exit.");
     let mut vectors: Vec<(Vector, Vector)> = Vec::new();
-	loop {
-		print!("> ");
-		io::stdout().flush().unwrap();
-		let line = read();
-		vectors.push(parse(line));
-		if vectors.len() > 1 {
-			let point = intersection(&vectors);
-			println!("The intersection is at ({:.1}, {:.1}).", point.x, point.y);
-		}
-	}
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let line = read();
+        if let Some(v) = parse(line) {
+            vectors.push(v);
+            if vectors.len() > 1 {
+                let point = intersection(&vectors);
+                println!("The intersection is at ({:.1}, {:.1}).", point.x, point.y);
+            }
+        }
+    }
 }
 
 fn intersection(vectors: &Vec<(Vector, Vector)>) -> Vector {
-	let len = vectors.len();
-	let l1 = solve_line(&vectors[len-1]);
-	let l2 = solve_line(&vectors[len-2]);
-	solve_intersection(&(l1, l2))
+    let len = vectors.len();
+    let l1 = solve_line(&vectors[len - 1]);
+    let l2 = solve_line(&vectors[len - 2]);
+    solve_intersection(&(l1, l2))
 }
 
 fn read() -> String {
@@ -30,15 +32,32 @@ fn read() -> String {
     line
 }
 
-fn parse(line: String) -> (Vector, Vector) {
+fn parse(line: String) -> Option<(Vector, Vector)> {
     let mut nums: Vec<f64> = Vec::new();
     for word in line.trim().split(" ") {
-        nums.push(word.parse().unwrap());
+        match word.parse() {
+            Ok(n) => nums.push(n),
+            Err(_) => (),
+        }
     }
-    (Vector{x: nums[0], y: nums[1]}, Vector{x: nums[2], y: nums[3]})
+    if nums.len() != 4 {
+        println!("Please input exactly 4 number");
+        return None;
+    }
+    Some((
+        Vector {
+            x: nums[0],
+            y: nums[1],
+        },
+        Vector {
+            x: nums[2],
+            y: nums[3],
+        },
+    ))
 }
 
-#[derive(Debug)] struct Vector {
+#[derive(Debug)]
+struct Vector {
     x: f64,
     y: f64,
 }
@@ -50,17 +69,17 @@ impl PartialEq for Vector {
 }
 
 fn solve_line(vectors: &(Vector, Vector)) -> Vector {
-	let (a, b) = vectors;
+    let (a, b) = vectors;
     let m = (a.y - b.y) / (a.x - b.x);
     let h = a.y - m * a.x;
-    Vector{x: m, y: h}
+    Vector { x: m, y: h }
 }
 
 fn solve_intersection(vectors: &(Vector, Vector)) -> Vector {
-	let (a, b) = vectors;
-    let m = - (a.y - b.y) / (a.x - b.x);
+    let (a, b) = vectors;
+    let m = -(a.y - b.y) / (a.x - b.x);
     let h = a.y + m * a.x;
-    Vector{x: m, y: h}
+    Vector { x: m, y: h }
 }
 
 #[cfg(test)]
@@ -69,32 +88,48 @@ mod tests {
 
     #[test]
     fn test_solve_line() {
-        let a = Vector{x: 0.0, y: 0.0};
-        let b = Vector{x: 1.0, y: 1.0};
-        let ans = Vector{x: 1.0, y: 0.0};
+        let a = Vector { x: 0.0, y: 0.0 };
+        let b = Vector { x: 1.0, y: 1.0 };
+        let ans = Vector { x: 1.0, y: 0.0 };
         assert_eq!(solve_line(&(a, b)), ans);
     }
 
     #[test]
     fn test_solve_intersection() {
-        let a = Vector{x: 1.0, y: 1.0};
-        let b = Vector{x: -1.0, y: -1.0};
-        let ans = Vector{x: -1.0, y: 0.0};
+        let a = Vector { x: 1.0, y: 1.0 };
+        let b = Vector { x: -1.0, y: -1.0 };
+        let ans = Vector { x: -1.0, y: 0.0 };
         assert_eq!(solve_intersection(&(a, b)), ans);
     }
 
     #[test]
     fn test_parse() {
-        let s = "20 8 2 3\n".to_owned();
-        assert_eq!(parse(s), (Vector{x: 20.0, y: 8.0}, Vector{x: 2.0, y: 3.0}));
+        let t = vec![
+            (
+                "20 8 2 3\n",
+                Some((Vector { x: 20.0, y: 8.0 }, Vector { x: 2.0, y: 3.0 })),
+            ),
+            (
+                "  1 2 3 4  \n",
+                Some((Vector { x: 1.0, y: 2.0 }, Vector { x: 3.0, y: 4.0 })),
+            ),
+            (
+                "1  2  3  4  \n",
+                Some((Vector { x: 1.0, y: 2.0 }, Vector { x: 3.0, y: 4.0 })),
+            ),
+            ("1 2 3 4 5 \n", None),
+        ];
+        for (q, ans) in t {
+            assert_eq!(parse(q.to_owned()), ans);
+        }
     }
 
-	#[test]
-	fn test_intersection() {
-		let vectors = vec![
-			(Vector{x: 1.0, y: 1.0}, Vector{x: 3.0, y: 1.0}), // y = 1
-			(Vector{x: 2.0, y: 2.0}, Vector{x: 4.0, y: 0.0}), // y = -x + 4
-		];
-		assert_eq!(intersection(&vectors), Vector{x: 3.0, y: 1.0});
-	}
+    #[test]
+    fn test_intersection() {
+        let vectors = vec![
+            (Vector { x: 1.0, y: 1.0 }, Vector { x: 3.0, y: 1.0 }), // y = 1
+            (Vector { x: 2.0, y: 2.0 }, Vector { x: 4.0, y: 0.0 }), // y = -x + 4
+        ];
+        assert_eq!(intersection(&vectors), Vector { x: 3.0, y: 1.0 });
+    }
 }
